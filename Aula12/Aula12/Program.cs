147c07a2b657ab7362cb1b213375597177a95423
matheus0321 <mtbveloso0321@gmail.com>
2020-04-23 19:57:02 -0300

    Add files via upload

---
+++ Aula12/Aula12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+           int anoNasc;
+            int anoAtual = 2020;
+           
+            Console.Write("Digite seu Nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite o ano que Você nasceu: ");
+            anoNasc = int.Parse(Console.ReadLine());
+            anoNasc= anoAtual - anoNasc;
+            
+            if (anoNasc >= 18)
+            {
+
+                Console.WriteLine("Ola {0} , você tem {1} anos , e você e maior de idade", nome, anoNasc);
+            }
+            else {
+                Console.WriteLine("Ola {0} , você tem {1} anos , e você e menoo de idade ", nome, anoNasc);
+               }
+
+            Console.ReadKey();
+        }
+    }
+}
